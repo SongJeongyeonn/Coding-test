6a4fd9f7182f18b8e825ce800eeaaf9a91eca3ab
jeongyeon0000 <153992648+jeongyeon0000@users.noreply.github.com>
2024-02-05 11:54:36 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+    int A;
+    scanf("%d", &A);
+    for(int i=0; i<A; i++)
+    {
+     for(int j=A; j < (A+i); j++)
+    {
+        printf(" ");
+    }
+    for(int l=1; l <= (A-i); l++)
+    {
+        printf("*");
+    }
+    for(int k=1; k < (A-i); k++)
+    {
+        printf("*");
+    }
+        printf("\n");
+    }
+    return 0;
+}
