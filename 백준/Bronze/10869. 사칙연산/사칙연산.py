002fd97c2670f9aef5e1e2f588312a2da2bbc4ea
jeongyeon0000 <153992648+jeongyeon0000@users.noreply.github.com>
2024-03-19 20:49:54 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,6 +1,2 @@
-a,b = map(int,input().split())
-print(a + b)
-print(a - b)
-print(a * b)
-print((int)(a / b))
-print(a % b)
+a,b = map(int, input().split())
+print(a+b, a-b, a*b, (int)(a/b), a%b, sep='\n')
