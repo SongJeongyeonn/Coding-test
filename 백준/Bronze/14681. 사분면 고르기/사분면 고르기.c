4777f3a642d298d637091bce2222f7ae1eaebf57
jeongyeon0000 <153992648+jeongyeon0000@users.noreply.github.com>
2024-03-21 16:54:38 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -1,23 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    if(A>0 && B>0)
-    {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    if(x>0 && y>0)
         printf("1");
-    }
-    else if(A<0 && B>0)
-    {
+    else if(x<0 && y>0)
         printf("2");
-    }
-    else if(A<0 && B<0)
-    {
+    else if(x<0 && y<0)
         printf("3");
-    }
     else
-    {
         printf("4");
-    }
     return 0;
 }
