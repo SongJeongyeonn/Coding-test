d029584c44ecb9b1910b313f5fc0cbb137265573
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-05-01 16:22:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6){ // 4와 6이 아닐때 false를 준다.
+            answer = false;
+        }
+        else{
+            for(int i = 0; i < s.length(); i++){// 인덱스는 0부터 시작하기에, i의 초기값을 0으로 준다.
+                if(s.charAt(i) >= '0' && s.charAt(i) <= '9') { // 각 인덱스의 값이 조건에 맞는 숫자인지 확인한다.
+                }
+                else{ // 아닐 시 false를 준다.
+                    answer = false;
+                }
+            }
+        }
+        return answer; 
+    }
+}
