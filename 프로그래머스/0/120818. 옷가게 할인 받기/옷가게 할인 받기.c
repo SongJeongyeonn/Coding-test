235e28d8d6998ef7c744c56ddd1e8de13473dbd6
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-06-26 12:23:39 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int solution(int price) {
+    int answer = price;
+    if(price >= 500000)
+        answer -= (price*0.2);
+    else if(price >= 300000)
+        answer -= (price*0.1);
+    else if(price >= 100000)
+        answer -= (price*0.05);
+    return answer;
+}
