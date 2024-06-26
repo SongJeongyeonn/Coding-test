822b582ecf93c5ee512aacb4cce5e766a1de9889
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-06-26 12:26:33 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.c
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 char* solution(const char* n_str) {
-    int i, cnt = 0;
-    char* answer = (char*)malloc(sizeof(char) * (strlen(n_str)+1));
-    for(i = 0; i < strlen(n_str); i++){
+    int i, cnt = 0, len = strlen(n_str);
+    char* answer = (char*)malloc(sizeof(char) * (len+1));
+    for(i = 0; i < len; i++){
         if(n_str[i] != '0')
             break;
     }
-    for(int j = i; j < strlen(n_str); j++){
+    for(int j = i; j < len; j++){
         answer[cnt] = n_str[j];
         cnt++;
     }
