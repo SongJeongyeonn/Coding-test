41ad04cafa2ce3d96981f0c26cd03b6bbb13064c
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-07-01 01:11:30 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int solution(int hp) {
+    int answer = 0;
+    while(hp > 0){
+        if(hp >= 5)
+            hp -= 5;
+        else if(hp >= 3)
+            hp -= 3;
+        else
+            hp -= 1;
+        answer++;
+    }
+    return answer;
+}
