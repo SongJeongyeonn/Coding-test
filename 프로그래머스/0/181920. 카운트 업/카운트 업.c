ee070286238acc47742b74dd29b3ba552bd070fd
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-07-23 06:00:40 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* solution(int start_num, int end_num) {
+    int* answer = (int*)malloc(sizeof(int)*(end_num-start_num+1));
+    for(int i = 0; i < end_num-start_num+1; i++){
+        answer[i] = start_num+i;
+    }
+    return answer;
+}
