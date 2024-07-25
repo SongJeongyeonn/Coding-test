e376e61ab8649806c1f5a4363ee20f1d74f87ccd
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-07-25 10:24:30 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    if(num_list_len > 10)
+        for(int i = 0; i < num_list_len; i++)
+            answer+=num_list[i];
+    else{
+        answer = 1;
+        for(int i = 0; i < num_list_len; i++)
+            answer*=num_list[i];
+    }
+    return answer;
+}
