5458cc17b63bc9b0d0ebf8da0ee4ca1fb64d65e0
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-08-11 23:42:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    long num[3] = { 0,1,0 };
+    if (n > 0)
+        for (int i = 0; i < n; i++) {
+            i % 2 != 0 ? (num[0] += num[1], num[2] = num[0]) : (num[1] += num[0], num[2] = num[1]);
+        }
+    printf("%ld", num[2]);
+    return 0;
+}
