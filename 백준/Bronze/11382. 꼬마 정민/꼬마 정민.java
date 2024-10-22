ea9a37edc9335459797a14f29ed61d036b778a2c
정연 <153992648+jeongyeon0000@users.noreply.github.com>
2024-10-22 10:03:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Long a = in.nextLong(), b = in.nextLong(), c = in.nextLong();
+        System.out.print(a+b+c);
+    }
+}
