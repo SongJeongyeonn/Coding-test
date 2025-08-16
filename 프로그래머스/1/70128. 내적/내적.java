addaca7ae3a87ed71019871b49083498ae695867
송정연 <153992648+SongJeongyeonn@users.noreply.github.com>
2025-08-16 10:10:31 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0, size = a.length;
+        for(int i = 0; i < size; i++){
+            answer = a[i] * b[i] + answer;
+        }
+        return answer;
+    }
+}
