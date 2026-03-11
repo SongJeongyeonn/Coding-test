cac826321c67507acf37547496c86ba6b0cfaae4
송정연 <153992648+SongJeongyeonn@users.noreply.github.com>
2026-03-11 10:37:22 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,6 +1,4 @@
 n = int(input())
 numbers = list(map(int, input().split()))
-result = 0
-for score in numbers:
-    result += score/max(numbers)*100
+result = sum(numbers)/max(numbers)*100
 print(result/n)
