777efa5afe762ea929a57c9bb1ca4883f20a92a5
송정연 <153992648+SongJeongyeonn@users.noreply.github.com>
2026-03-13 09:22:33 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 n = int(input())
 people = list(map(int, input().split()))
 people.sort()
-res = 0
 for i in range(1, n):
     people[i] += people[i-1]
 print(sum(people))
