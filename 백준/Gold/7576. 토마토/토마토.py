3c1184f1df74494c388d53031a84b865d62edf29
송정연 <153992648+SongJeongyeonn@users.noreply.github.com>
2026-03-27 10:03:02 +0900

    [Gold V] Title: 토마토, Time: 2440 ms, Memory: 100472 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -6,11 +6,15 @@ m,n = map(int, input().split())
 tomato = [list(map(int, input().split())) for _ in range(n)]
 q = deque()
 dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+cnt1 = 0
+cnt2 = 0
 
 for i in range(n):
     for j in range(m):
         if tomato[i][j] == 1:
             q.append((i, j)) # 1 위치 찾아서 시작지점
+        elif tomato[i][j] == 0:
+            cnt1 += 1
 
 while q:
     x, y = q.popleft() # 위치 꺼내기
@@ -18,15 +22,15 @@ while q:
         nx, ny = x + dx, y + dy
         if 0 <= nx < n and 0 <= ny < m: # 유효 범위
             if tomato[nx][ny] == 0: # 안 익은 거만
+                cnt2 += 1
                 tomato[nx][ny] = tomato[x][y] + 1 # 날짜 더하기(누적됨)
                 q.append((nx, ny)) # 다시 저장.
 
 result = 0
-for i in range(n):
-    for j in range(m):
-        if tomato[i][j] == 0: # 토마토가 모두 못익는 상황(-1로 둘러쌓여 익지 못한 토마토)
-            print(-1)
-            exit(0)
-        result = max(result, tomato[i][j]) # 누적값이 큰걸로 교체
-
-print(result - 1)
+if cnt1 != cnt2:
+    print(-1)
+else:
+    for i in range(n):
+        for j in range(m):
+            result = max(result, tomato[i][j]) # 누적값이 큰걸로 교체
+    print(result - 1)
