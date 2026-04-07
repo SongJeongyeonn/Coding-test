8a30bbbb67f77a151382614aa51e6f4926695c25
송정연 <153992648+SongJeongyeonn@users.noreply.github.com>
2026-04-08 08:48:55 +0900

    [level 1] Title: 덧칠하기, Time: 14.95 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,11 +1,8 @@
 def solution(n, m, section):
-    # m 크기만큼 색칠, n 개수만큼  칠하기, section 칠하는 최수값 return 
     answer = 1
     cnt = 0
     for i in range(1,len(section)):
-        if section[cnt]-(m-1) <= section[i] <= section[cnt]+(m-1):
-            continue;
-        else:
+        if not(section[cnt]-(m-1) <= section[i] <= section[cnt]+(m-1)):
             answer+=1
             cnt = i
     return answer
